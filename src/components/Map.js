5c393ec328f5ba90453f859e72bcf08843821aa9
Tarin <tarin.eccleston@gmail.com>
2022-02-17 23:19:42 +1300

    Restructure. Get Google Maps working

---
+++ src/components/Map.js
@@ -0,0 +1,24 @@
+import GoogleMapReact from 'google-map-react'
+
+const Map = ({ center, zoom }) => {
+  return (
+    <div className="map">
+      <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyCK2eAYFSo7KaKJCSQjzkhb2fJYSWlM_TA' }}
+          defaultCenter={ center }
+          defaultZoom={ zoom }
+      >
+      </GoogleMapReact>
+    </div>
+  )
+}
+
+Map.defaultProps = {
+    center: {
+        lat: 42.3265,
+        lng: -122.8756
+    },
+    zoom: 10
+}
+
+export default Map
