18f6c32e94534e7b4a10160e59b24fb79f510401
Tarin <tarin.eccleston@gmail.com>
2022-02-18 15:06:45 +1300

    Server active

--- server/server.py
+++ server/server.py
@@ -1,3 +1,4 @@
+import flask
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,5 +6,5 @@ app = Flask(__name__)
 @app.route('/server', methods=['GET'])
 def server():
     return {
-        id: '345'
+        'id': '345'
     }
