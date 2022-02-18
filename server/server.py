fc7214bb29916f052f7d356180d5fc4b58e923fb
Tarin <tarin.eccleston@gmail.com>
2022-02-18 15:12:16 +1300

    Import ISS dependencies

--- server/server.py
+++ server/server.py
@@ -1,10 +1,19 @@
 import flask
 from flask import Flask
 
+import time
+import math
+import datetime
+import requests
+import skyfield
+from skyfield.api import EarthSatellite, wgs84
+
 app = Flask(__name__)
 
 @app.route('/server', methods=['GET'])
 def server():
     return {
-        'id': '345'
+        'lat': 1,
+        'lng': 2,
+        'alt': 3
     }
