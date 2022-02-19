1b0459d79697fa71f6cdf12a80e0efd8243163b6
Tarin <tarin.eccleston@gmail.com>
2022-02-19 13:25:20 +1300

    Successful communication betwee front-end and server

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import Map from './components/Map'
+import React, {useState, useEffect} from 'react'
 
 function App() {
+  const [initialData, setInitialData] = useState([{}])
+
+  useEffect(() => {
+    fetch('/server').then(
+      response => response.json()
+    ).then(data => setInitialData(data))
+  }, []);
+
   return (
     <div>
-      <Map />
+      {/* <Map /> */}
+      <h1>{initialData.alt}</h1>
     </div>
   );
 }
